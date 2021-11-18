58bc42f6e099ddad4a3c1a3d6455a002c87745d0
Álvaro Fernández <avunotfound@gmail.com>
2021-11-18 12:47:01 +0100

    PIIINGU PIIINGU

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -4,54 +4,72 @@ import Manager.DBManager;
 import Menu.Menu;
 
 import java.util.Scanner;
-
+//opciones:
+//    Ejecutar cconsulta
+//    Ejecutar DML
+//    Ejecutar DDL
+//    Configuracion de la conexion
+//    Salir del programa
 public class Main {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
-        DBManager.iniValues();
-        if(DBManager.createConnection()!=null)
-            Menu.mainMenu();
-        else{
-            Menu.configMenu();
-            String userChoice="";
-            while (!userChoice.equals("6")){
-                userChoice=sc.next();
-                switch (userChoice) {
-                    case "1" -> changeUser(sc);
-                    case "2" -> changePass(sc);
-                    case "3" -> changeDB(sc);
-                    case "4" -> changePort(sc);
-                    case "5" -> changeHost(sc);
+        DBManager.iniProperties();
+//        if(DBManager.createConnection()!=null)
+//            Menu.mainMenu();
+//        else{
+//            configMain(sc);
+//        }
+        String menuOption= "";
+        while (menuOption.equals("")) {
+            menuOption= sc.next();
+            switch (menuOption) {
+                case "1" -> {
                 }
-            DBManager.setUrl();
+                case "2" -> {
+                }
+                case "3" -> {
+                }
+                case "4" -> configMain(sc);
+                case "5" -> {
+                }
             }
         }
     }
-
+    private static void configMain(Scanner sc) {
+        String userChoice="";
+        while (!userChoice.equals("6")){
+            Menu.configMenu();
+            userChoice=sc.next();
+            switch (userChoice) {
+                case "1" -> changeUser(sc);
+                case "2" -> changePass(sc);
+                case "3" -> changeDB(sc);
+                case "4" -> changePort(sc);
+                case "5" -> changeHost(sc);
+            }
+            DBManager.setUrl();
+        }
+    }
     private static void changeHost(Scanner sc) {
         Menu.askHost();
         String input = sc.next();
         DBManager.setHost(input);
     }
-
     private static void changePort(Scanner sc) {
         Menu.askPort();
         String input = sc.next();
         DBManager.setPort(input);
     }
-
     private static void changeDB(Scanner sc) {
         Menu.askDB();
         String input = sc.next();
         DBManager.setDataBase(input);
     }
-
     private static void changePass(Scanner sc) {
         Menu.askPassword();
         String input = sc.next();
         DBManager.setPass(input);
     }
-
     private static void changeUser(Scanner sc) {
         Menu.askUser();
         String input = sc.next();
--- src/Manager/DBManager.java
+++ src/Manager/DBManager.java
@@ -7,113 +7,123 @@ import java.sql.*;
 import java.util.Properties;
 
 public class DBManager {
-    //opciones:
-//    Ejecutar cconsulta
-//    Ejecutar DML
-//    Ejecutar DDL
-//    Configuracion de la conexion
-//    Salir del programa
 
     private static Properties properties= new Properties();
-    private static String driver=properties.getProperty("driver");
-    private static String host=properties.getProperty("host");
-    private static String port=properties.getProperty("port");
-    private static String dataBase=properties.getProperty("database");
-    private static String user=properties.getProperty("user");
-    private static String pass=properties.getProperty("pass");
-    private static String url=driver + host + ":" + port + ";databaseName=" + dataBase+ ";integratedSecurity=false";
-    private static OutputStream os;
+    private static String host;
+    private static String port;
+    private static String dataBase;
+    private static String user;
+    private static String pass;
+    private static String url;
 
-    public static void iniValues()
-    {
-        properties.setProperty("driver","jdbc:sqlserver://");
-        properties.setProperty("host", "localhost");
-        properties.setProperty("port","49674");
-        properties.setProperty("database","DataExample");
-        properties.setProperty("user", "sa");
-        properties.setProperty("pass", "mitesoro");
+    public static void iniProperties() {
+        setProperties();
+        getPropertiesValues();
         try {
-            properties.store(os= new FileOutputStream(""),null);
+            OutputStream os;
+            properties.store(os = new FileOutputStream("Config"),null);
         } catch (IOException e) {
             e.printStackTrace();
         }
+        setUrl();
     }
+    private static void getPropertiesValues() {
+        host=properties.getProperty("host");
+        port=properties.getProperty("port");
+        dataBase=properties.getProperty("database");
+        user=properties.getProperty("user");
+        pass=properties.getProperty("pass");
+    }
+    private static void setProperties() {
+        properties.setProperty("host", "localhost");
+        properties.setProperty("port", "49674");
+        properties.setProperty("database","DataExample");
+        properties.setProperty("user", "sa");
+        properties.setProperty("pass", "mitesoro");
+    }
+
     public static String getHost() {
         return host;
     }
-
     public static void setHost(String host) {
         DBManager.properties.setProperty("host", host);
     }
-
     public static String getPort() {
         return port;
     }
-
     public static void setPort(String port) {
         DBManager.properties.setProperty("port", port);
     }
-
     public static String getDataBase() {
         return dataBase;
     }
-
     public static void setDataBase(String dataBase) {
         DBManager.properties.setProperty("database", dataBase);
     }
-
     public static String getUser() {
         return user;
     }
-
     public static void setUser(String user) {
         DBManager.properties.setProperty("user", user);
     }
-
     public static String getPass() {
         return pass;
     }
-
     public static void setPass(String pass) {
         DBManager.properties.setProperty("pass", pass);
     }
-
     public static String getUrl() {
         return url;
     }
-
     public static void setUrl() {
-        url=driver + host + ":" + port + ";databaseName=" + dataBase+ ";integratedSecurity=false";
+        url="jdbc:sqlserver://" + host + ":" + port + ";databaseName=" + dataBase+ ";integratedSecurity=false";
     }
 
-    public static Connection createConnection()
+    public void executeQuery(String sqlQuery)
     {
+        Connection connection= createConnection();
+        try (Statement stat = connection.createStatement()) {
+            ResultSet rs= stat.executeQuery(sqlQuery);
+            while(rs.next())
+            {
+            }
+            close(rs);
+            close(stat);
+        }catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        close(connection);
+    }
+    public void executeDML()
+    {
+
+    }
+
+    public static Connection createConnection() {
         Connection connection = null;
         try {
             connection= DriverManager.getConnection(url, user, pass);
         } catch (SQLException e) {
-            System.err.println("F");
+            e.printStackTrace();
         }
         return connection;
     }
-    public static void close(ResultSet result)
-    {
+    public static void close(ResultSet result) {
         try {
             result.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
-    public static void close(Statement statement)
-    {
+    public static void close(Statement statement) {
         try {
             statement.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
-    public static void close(Connection connection)
-    {
+    public static void close(Connection connection) {
         try {
             connection.close();
         } catch (SQLException e) {
