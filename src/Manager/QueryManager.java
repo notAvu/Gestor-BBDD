3cddce63639fd3c2778b2d901b7d15ee8e18392e
Álvaro Fernández <avunotfound@gmail.com>
2021-12-02 12:41:14 +0100

    Vaya

--- src/Manager/QueryManager.java
+++ src/Manager/QueryManager.java
@@ -1,4 +1,44 @@
 package Manager;
 
+import java.io.*;
+
 public class QueryManager {
+    File sqlFile;
+    FileWriter fw;
+    FileReader fr;
+    BufferedWriter bw;
+    BufferedReader br;
+
+    public QueryManager(File sqlFile, FileWriter fw, BufferedWriter bw, BufferedReader br, FileReader fr) {
+        this.sqlFile = sqlFile;
+        this.fw = fw;
+        this.bw = bw;
+        this.br = br;
+        this.fr = fr;
+    }
+
+    public void readQuery()
+    {
+        String query;
+
+    }
+    public QueryManager(String filename) {
+        this.sqlFile = new File(filename);
+        iniReader();
+    }
+
+    private void iniReader() {
+//        try {
+//            fw = new FileWriter(sqlFile);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        bw = new BufferedWriter(fw);
+        try {
+            fr = new FileReader(sqlFile);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        br = new BufferedReader(fr);
+    }
 }
